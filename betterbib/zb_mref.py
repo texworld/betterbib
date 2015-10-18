cc3e13c7f2faf0b89fd1e44be407b5d1ac68ea7e
Nico Schlömer <nico.schloemer@gmail.com>
2015-10-19 00:19:25 +0200

    remove redundant testing code from zb_mref

--- betterbib/zb_mref.py
+++ betterbib/zb_mref.py
@@ -30,48 +30,6 @@ class ZentralblattMref(Source):
     '''
 
     def __init__(self):
-
-        return
-
-    def _test_connection(self):
-        '''Test the connection with Zentralblatt/Mref.
-        '''
-        test_entry = {
-                'title': 'Krylov subspace methods',
-                'author': 'Liesen and Strakoš',
-                'year': '2013'
-                }
-
-        # Define the expected return string. Note that special characters need
-        # to be escaped.
-        expected = '''@book {MR3024841,
-    AUTHOR = {Liesen, J{\\"o}rg and Strako{\\v{s}}, Zden{\\v{e}}k},
-     TITLE = {Krylov subspace methods},
-    SERIES = {Numerical Mathematics and Scientific Computation},
-      NOTE = {Principles and analysis},
- PUBLISHER = {Oxford University Press, Oxford},
-      YEAR = {2013},
-     PAGES = {xvi+391},
-      ISBN = {978-0-19-965541-0},
-   MRCLASS = {65F10 (65F15)},
-  MRNUMBER = {3024841},
-MRREVIEWER = {Melina A. Freitag},
-}'''
-        bt = self.find(test_entry)
-        # Check the result.
-        if bt != expected:
-            diff = difflib.Differ().compare(bt, expected)
-            diff = ''.join([
-                '***' + i[2:] + '***' if i[:1] == '+'
-                else i[2:] for i in diff if not i[:1] in '-?'
-                ])
-            print
-            print('Unexpected test result. Differences:')
-            print(diff)
-            print
-            raise RuntimeError(
-                'Connection established, but wrong search result.'
-                )
         return
 
     def find(self, entry):
