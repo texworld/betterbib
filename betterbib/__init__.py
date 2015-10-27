3d854c2c8f8794d0a3e2a516b0b71c7a55867688
Nico Schlömer <nico.schloemer@gmail.com>
2015-10-27 14:57:43 +0100

    bump the version number to 1.0.0

--- betterbib/__init__.py
+++ betterbib/__init__.py
@@ -21,7 +21,7 @@ __all__ = [
     ]
 
 __name__ = 'betterbib'
-__version__ = '0.4.0'
+__version__ = '1.0.0'
 __author__ = 'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
 __website__ = 'https://github.com/nschloe/betterbib'
