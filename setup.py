d4b16548458448563d71ec6c2beb7cfd703453db
Nico Schlömer <nico.schloemer@gmail.com>
2015-10-27 16:38:42 +0100

    add python 2, 3 explcitly to support list

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Utilities'
         ],
     scripts=[
