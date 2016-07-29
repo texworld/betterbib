6fe87c78bd428d415004ac43414bb823056e8624
Nico Schlömer <nico.schloemer@gmail.com>
2016-07-29 20:14:20 +0200

    remove mathscinet source
    It's nonfree, hardly testable, and an ugly HTML hack.

--- betterbib/__init__.py
+++ betterbib/__init__.py
@@ -5,7 +5,6 @@ from betterbib.bibtex import pybtex_to_dict, \
         latex_to_unicode
 from betterbib.crossref import Crossref
 from betterbib.dictdiffer import adds_info
-from betterbib.mathscinet import MathSciNet
 from betterbib.progress_bar import ProgressBar
 from betterbib.source import Source
 from betterbib.zb_mref import ZentralblattMref
@@ -14,7 +13,6 @@ __all__ = [
     'bibtex',
     'crossref',
     'dictdiffer',
-    'mathscinet',
     'progress_bar',
     'source',
     'zb_mref'
