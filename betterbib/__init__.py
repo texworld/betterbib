728e44b2ef74ed40bc00f009d5196068c0e87d22
Nico Schlömer <nico.schloemer@gmail.com>
2016-07-29 23:35:23 +0200

    replace custom progressbar by tqdm

--- betterbib/__init__.py
+++ betterbib/__init__.py
@@ -5,13 +5,11 @@ from betterbib.bibtex import pybtex_to_dict, \
         latex_to_unicode
 from betterbib.crossref import Crossref
 from betterbib.dictdiffer import adds_info
-from betterbib.progress_bar import ProgressBar
 
 __all__ = [
     'bibtex',
     'crossref',
     'dictdiffer',
-    'progress_bar',
     ]
 
 __version__ = '1.1.2'
