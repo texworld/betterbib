2ca5ac845099758d961a9380e6d75b8bb4957cee
Nico Schlömer <nico.schloemer@gmail.com>
2016-08-28 22:53:34 +0200

    don't fail if book/incollection could not be guessed

--- betterbib/crossref.py
+++ betterbib/crossref.py
@@ -37,14 +37,12 @@ class Crossref(object):
 
             book_doi = a.group(1)
 
-            # Get the book data
+            # Try to get the book data
             r = requests.get(self.api_url + '/' + book_doi)
-            assert r.ok
-
-            book_data = r.json()
-
-            if 'author' in book_data['message']:
-                return 'inbook'
+            if r.ok:
+                book_data = r.json()
+                if 'author' in book_data['message']:
+                    return 'inbook'
             # else
             return 'incollection'
 
