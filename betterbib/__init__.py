ca29ebe65e2ec7043e371267d4a3392fada1d4df
Nico Schlömer <nico.schloemer@gmail.com>
2018-03-10 07:46:05 +0100

    update copyright year

--- betterbib/__init__.py
+++ betterbib/__init__.py
@@ -27,4 +27,4 @@ except ImportError:
     pass
 else:
     if pipdate.needs_checking(__name__):
-        print(pipdate.check(__name__, __version__))
+        print(pipdate.check(__name__, __version__), end='')
