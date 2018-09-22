cccf03cd8edd714c68e28079af55c5c9215f7f09
David W.H. Swenson <dwhs@hyperblazer.net>
2018-09-22 23:19:18 +0200

    make black happy

--- test/test_journal_abbrev.py
+++ test/test_journal_abbrev.py
@@ -6,16 +6,19 @@ from collections import namedtuple
 
 import betterbib
 
-FieldHolder = namedtuple('FieldHolder', 'fields')
+FieldHolder = namedtuple("FieldHolder", "fields")
 
 # the following are re-used in several tests
-FULL_PNAS = ("Proceedings of the National academy of Sciences "
-             "of the United States of America")
+FULL_PNAS = (
+    "Proceedings of the National academy of Sciences of the United States of America"
+)
 PNAS_ABBREV = "Proc. Natl. Acad. Sci. U.S.A."
 
-def make_fake_entry(journal, citekey='foo'):
-    contents = FieldHolder({'journal': journal})
+
+def make_fake_entry(journal, citekey="foo"):
+    contents = FieldHolder({"journal": journal})
     return {citekey: contents}
+
 
 def test_standard_abbrev():
     journals = [FULL_PNAS]
@@ -23,7 +26,8 @@ def test_standard_abbrev():
     for journal, abbrev in zip(journals, abbrevs):
         entries = make_fake_entry(journal)
         abbreviated = betterbib.journal_abbrev(entries)
-        assert abbreviated['foo'].fields['journal'] == abbrev
+        assert abbreviated["foo"].fields["journal"] == abbrev
+
 
 def test_custom_abbrev():
     infile = tempfile.NamedTemporaryFile().name
@@ -31,9 +35,9 @@ def test_custom_abbrev():
     with open(infile, "w") as f:
         f.write(super_short)
 
-    entries = make_fake_entry('PNAS')
+    entries = make_fake_entry("PNAS")
     abbreviated = betterbib.journal_abbrev(entries, custom_abbrev=infile)
-    assert abbreviated['foo'].fields['journal'] == PNAS_ABBREV
+    assert abbreviated["foo"].fields["journal"] == PNAS_ABBREV
 
     super_short = '{"' + FULL_PNAS + '": "PNAS"}'
     with open(infile, "w") as f:
@@ -41,16 +45,17 @@ def test_custom_abbrev():
 
     entries = make_fake_entry(FULL_PNAS)
     abbreviated = betterbib.journal_abbrev(entries, custom_abbrev=infile)
-    assert abbreviated['foo'].fields['journal'] == "PNAS"
+    assert abbreviated["foo"].fields["journal"] == "PNAS"
+
 
 def test_standard_abbrev_long():
     journals = [FULL_PNAS]
     abbrevs = [PNAS_ABBREV]
     for journal, abbrev in zip(journals, abbrevs):
         entries = make_fake_entry(abbrev)
-        abbreviated = betterbib.journal_abbrev(entries,
-                                               long_journal_names=True)
-        assert abbreviated['foo'].fields['journal'] == journal
+        abbreviated = betterbib.journal_abbrev(entries, long_journal_names=True)
+        assert abbreviated["foo"].fields["journal"] == journal
+
 
 def test_custom_abbrev_long():
     infile = tempfile.NamedTemporaryFile().name
@@ -59,7 +64,5 @@ def test_custom_abbrev_long():
         f.write(super_short)
 
     entries = make_fake_entry(PNAS_ABBREV)
-    abbreviated = betterbib.journal_abbrev(entries,
-                                           long_journal_names=True)
-    assert abbreviated['foo'].fields['journal'] == FULL_PNAS
-
+    abbreviated = betterbib.journal_abbrev(entries, long_journal_names=True)
+    assert abbreviated["foo"].fields["journal"] == FULL_PNAS
