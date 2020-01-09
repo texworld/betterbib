a7604a1a0a15199af96d4fa7f572376a5ac4cf32
Nico Schlömer <nico.schloemer@gmail.com>
2020-01-09 17:43:16 +0100

    version bump, year bump

--- betterbib/__about__.py
+++ betterbib/__about__.py
@@ -1,7 +1,7 @@
-__version__ = "3.5.2"
+__version__ = "3.5.3"
 __author__ = "Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
 __website__ = "https://github.com/nschloe/betterbib"
-__copyright__ = "Copyright (c) 2013-2019, {} <{}>".format(__author__, __author_email__)
+__copyright__ = "Copyright (c) 2013-2020, {} <{}>".format(__author__, __author_email__)
 __status__ = "Development Status :: 5 - Production/Stable"
 __license__ = "License :: OSI Approved :: MIT License"
