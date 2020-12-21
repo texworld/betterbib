bdef24c4993cb485bc21ac7f432dd34c4f5bce2b
Nico Schlömer <nico.schloemer@gmail.com>
2020-12-21 11:47:54 +0100

    meta updates

--- betterbib/__init__.py
+++ betterbib/__init__.py
@@ -3,6 +3,7 @@ from .__about__ import __version__
 from .adapt_doi_urls import adapt_doi_urls
 from .crossref import Crossref
 from .dblp import Dblp
+from . import errors
 from .journal_abbrev import journal_abbrev
 from .sync import sync
 from .tools import (
@@ -20,6 +21,7 @@ __all__ = [
     "cli",
     "create_dict",
     "decode",
+    "errors",
     "pybtex_to_dict",
     "pybtex_to_bibtex_string",
     "write",
