a987fc12f437c5f0212335c02e28afbf902bb3e1
Nico Schlömer <nico.schloemer@gmail.com>
2021-07-21 21:21:01 +0200

    unify some tests

--- tests/test_bibtex_title.py
+++ tests/test_bibtex_title.py
@@ -1,76 +1,47 @@
+import pytest
+
 import betterbib
 
 
-def test_translate_title_capitalized():
-    assert (
-        betterbib.tools._translate_title(
-            "The Magnus expansion and some of its applications"
-        )
-        == "The {Magnus} expansion and some of its applications"
-    )
-
-    assert (
-        betterbib.tools._translate_title(
-            "On generalized averaged Gaussian formulas, II"
-        )
-        == "On generalized averaged {Gaussian} formulas, {II}"
-    )
-
-    assert (
-        betterbib.tools._translate_title("Gaussian Hermitian Jacobian")
-        == "{Gaussian} {Hermitian} {Jacobian}"
-    )
-
-
-def test_translate_title_ACRONYM():
-    assert (
-        betterbib.tools._translate_title("VODE: a variable-coefficient ODE solver")
-        == "{VODE:} {A} variable-coefficient {ODE} solver"
-    )
-
-    assert (
-        betterbib.tools._translate_title(
-            "GMRES: A generalized minimal residual algorithm"
-        )
-        == "{GMRES:} {A} generalized minimal residual algorithm"
-    )
-
-
-def test_translate_title_apostrophe():
-    assert (
-        betterbib.tools._translate_title(
-            "Peano's kernel theorem for vector-valued functions"
-        )
-        == "{Peano's} kernel theorem for vector-valued functions"
-    )
-
-
-def test_translate_title_dashed_names():
-    assert (
-        betterbib.tools._translate_title(
-            "Exponential Runge-Kutta methods for parabolic problems"
-        )
-        == "Exponential {Runge}-{Kutta} methods for parabolic problems"
-    )
-
-
-def test_translate_title_dashes():
-    assert (
-        betterbib.tools._translate_title("Dash-Dash Double--Dash Triple---Dash")
-        == "Dash-Dash Double--Dash Triple---Dash"
-    )
-
-
-def test_translate_title_variable():
-    assert betterbib.tools._translate_title("x: {X}") == "x: {X}"
-
-
-def test_translate_title_escape_dollar():
-    assert (
-        betterbib.tools._translate_title("{Aaa ${\\text{Pt/Co/AlO}}_{x}$ aaa bbb}")
-        == "{Aaa {${\\text{Pt/Co/AlO}}_{x}$} aaa bbb}"
-    )
-
-
-def test_translate_title_star():
-    assert betterbib.tools._translate_title("z*") == "z*"
+@pytest.mark.parametrize(
+    "string,ref",
+    [
+        (
+            "The Magnus expansion and some of its applications",
+            "The {Magnus} expansion and some of its applications",
+        ),
+        (
+            "On generalized averaged Gaussian formulas, II",
+            "On generalized averaged {Gaussian} formulas, {II}",
+        ),
+        ("Gaussian Hermitian Jacobian", "{Gaussian} {Hermitian} {Jacobian}"),
+        (
+            "VODE: a variable-coefficient ODE solver",
+            "{VODE:} {A} variable-coefficient {ODE} solver",
+        ),
+        (
+            "GMRES: A generalized minimal residual algorithm",
+            "{GMRES:} {A} generalized minimal residual algorithm",
+        ),
+        (
+            "Peano's kernel theorem for vector-valued functions",
+            "{Peano's} kernel theorem for vector-valued functions",
+        ),
+        (
+            "Exponential Runge-Kutta methods for parabolic problems",
+            "Exponential {Runge}-{Kutta} methods for parabolic problems",
+        ),
+        (
+            "Dash-Dash Double--Dash Triple---Dash",
+            "Dash-Dash Double--Dash Triple---Dash",
+        ),
+        ("x: {X}", "x: {X}"),
+        (
+            "{Aaa ${\\text{Pt/Co/AlO}}_{x}$ aaa bbb}",
+            "{Aaa {${\\text{Pt/Co/AlO}}_{x}$} aaa bbb}",
+        ),
+        ("z*", "z*"),
+    ],
+)
+def test_translate_title(string, ref):
+    assert betterbib.tools._translate_title(string) == ref
