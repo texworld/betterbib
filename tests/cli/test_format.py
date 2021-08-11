b76d95bc3da13db91cf26abdca9a0ed6999279ef
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-11 21:03:05 +0200

    more simplification

--- tests/cli/test_format.py
+++ tests/cli/test_format.py
@@ -1,137 +1,88 @@
 import tempfile
 from pathlib import Path
 
+import pytest
+
 import betterbib
 
+version_line = (
+    f"%comment{{This file was created with betterbib v{betterbib.__version__}.}}\n"
+)
 
-def test_cli_format(capsys):
-    ref_test_cli_format_in = (
-        "@article{foobar,\n" "doi={foobar},\n" "url = {https://doi.org/foobar}\n" "}"
-    )
 
-    ref_test_cli_format_out = (
-        f"%comment{{This file was created with betterbib v{betterbib.__version__}.}}\n"
-        "\n"
-        "\n"
-        "@article{foobar,\n"
-        " doi = {foobar},\n"
-        " url = {https://doi.org/foobar},\n"
-        "}\n"
-    )
+@pytest.mark.parametrize(
+    "ref_in,ref_out",
+    [
+        (
+            "@article{foobar,\n"
+            "doi={foobar},\n"
+            "url = {https://doi.org/foobar}\n"
+            "}",
+            version_line + "\n"
+            "\n"
+            "@article{foobar,\n"
+            " doi = {foobar},\n"
+            " url = {https://doi.org/foobar},\n"
+            "}\n",
+        ),
+        # escape ampersand:
+        (
+            "@article{foobar,\n"
+            "doi={foobar},\n"
+            "url = {https://doi.org/foobar},\n"
+            r"title = {Foo & Bar}," + "\n"
+            "}",
+            version_line + "\n"
+            "\n"
+            "@article{foobar,\n"
+            " doi = {foobar},\n"
+            " url = {https://doi.org/foobar},\n"
+            r" title = {Foo \& Bar}," + "\n"
+            "}\n",
+        ),
+        # escape command:
+        (
+            "@article{foobar,\n"
+            "doi={foobar},\n"
+            "url = {https://doi.org/foobar},\n"
+            r"title = {Foo on \LaTeX}," + "\n"
+            "}",
+            version_line + "\n"
+            "\n"
+            "@article{foobar,\n"
+            " doi = {foobar},\n"
+            " url = {https://doi.org/foobar},\n"
+            r" title = {Foo on \LaTeX}," + "\n"
+            "}\n",
+        ),
+        # more escape:
+        (
+            "@article{foobar,\n"
+            "doi={foobar},\n"
+            "url = {https://doi.org/foobar},\n"
+            r"title = {Foo & Bar on \LaTeX\ @TheBridge}," + "\n"
+            "}",
+            version_line + "\n"
+            "\n"
+            "@article{foobar,\n"
+            " doi = {foobar},\n"
+            " url = {https://doi.org/foobar},\n"
+            r" title = {Foo \& Bar on \LaTeX @TheBridge}," + "\n"
+            "}\n",
+        ),
+    ],
+)
+def test_cli_format(ref_in, ref_out, capsys):
     with tempfile.TemporaryDirectory() as tmpdir:
         infile = Path(tmpdir) / "test.bib"
 
         with open(infile, "w") as f:
-            f.write(ref_test_cli_format_in)
+            f.write(ref_in)
 
         betterbib.cli.main(["format", str(infile)])
         captured = capsys.readouterr()
-        assert captured.out == ref_test_cli_format_out
+        assert captured.out == ref_out
 
         betterbib.cli.main(["format", "--in-place", str(infile)])
         with open(infile) as f:
-            assert f.read() == ref_test_cli_format_out
-
-
-def test_cli_format_escape_ampersand(capsys):
-    ref_in = (
-        "@article{foobar,\n"
-        "doi={foobar},\n"
-        "url = {https://doi.org/foobar},\n"
-        r"title = {Foo & Bar}," + "\n"
-        "}"
-    )
-    ref_out = (
-        f"%comment{{This file was created with betterbib v{betterbib.__version__}.}}\n"
-        "\n"
-        "\n"
-        "@article{foobar,\n"
-        " doi = {foobar},\n"
-        " url = {https://doi.org/foobar},\n"
-        r" title = {Foo \& Bar}," + "\n"
-        "}\n"
-    )
-    with tempfile.TemporaryDirectory() as tmpdir:
-        infile = Path(tmpdir) / "test.bib"
-
-        with open(infile, "w") as f:
-            f.write(ref_in)
-
-        betterbib.cli.main(["format", str(infile)])
-        captured = capsys.readouterr()
-        assert captured.out == "".join(ref_out)
-
-        for _ in range(5):
-            betterbib.cli.main(["format", "--in-place", str(infile)])
-            with open(infile) as f:
-                assert f.read() == ref_out
-
-
-def test_cli_format_escape_command(capsys):
-    ref_in = (
-        "@article{foobar,\n"
-        "doi={foobar},\n"
-        "url = {https://doi.org/foobar},\n"
-        r"title = {Foo on \LaTeX}," + "\n"
-        "}"
-    )
-    ref_out = (
-        f"%comment{{This file was created with betterbib v{betterbib.__version__}.}}\n"
-        "\n"
-        "\n"
-        "@article{foobar,\n"
-        " doi = {foobar},\n"
-        " url = {https://doi.org/foobar},\n"
-        r" title = {Foo on \LaTeX}," + "\n"
-        "}\n"
-    )
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        infile = Path(tmpdir) / "test.bib"
-
-        with open(infile, "w") as f:
-            f.write(ref_in)
-
-        betterbib.cli.main(["f", str(infile)])
-        captured = capsys.readouterr()
-        assert captured.out == ref_out
-
-        for _ in range(5):
-            betterbib.cli.main(["f", "--in-place", str(infile)])
-            with open(infile) as f:
-                assert f.read() == ref_out
-
-
-def test_cli_format_escape(capsys):
-    ref_in = (
-        "@article{foobar,\n"
-        "doi={foobar},\n"
-        "url = {https://doi.org/foobar},\n"
-        r"title = {Foo & Bar on \LaTeX\ @TheBridge}," + "\n"
-        "}"
-    )
-    ref_out = (
-        f"%comment{{This file was created with betterbib v{betterbib.__version__}.}}\n"
-        "\n"
-        "\n"
-        "@article{foobar,\n"
-        " doi = {foobar},\n"
-        " url = {https://doi.org/foobar},\n"
-        r" title = {Foo \& Bar on \LaTeX @TheBridge}," + "\n"
-        "}\n"
-    )
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        infile = Path(tmpdir) / "test.bib"
-
-        with open(infile, "w") as f:
-            f.write(ref_in)
-
-        betterbib.cli.main(["f", str(infile)])
-        captured = capsys.readouterr()
-        assert captured.out == ref_out
-
-        for _ in range(5):
-            betterbib.cli.main(["format", "--in-place", str(infile)])
-            with open(infile) as f:
-                assert f.read() == ref_out
+            assert f.read() == ref_out
