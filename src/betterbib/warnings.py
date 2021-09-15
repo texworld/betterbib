808096c17f6e51641313a507b7f21835c1ba104f
Jens Heinrich <github.com/JensHeinrich>
2021-09-15 10:47:20 +0200

    Add warning classes

---
+++ src/betterbib/warnings.py
@@ -0,0 +1,14 @@
+class UnsupportedType(Warning):
+    pass
+
+
+class UnsupportedBibTeXType(UnsupportedType):
+    pass
+
+
+class UnsupportedBibLaTeXType(UnsupportedType):
+    pass
+
+
+class UnsupportedCrossRefType(UnsupportedType):
+    pass
